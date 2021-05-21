0197b26622f3fd296254d35c5a5af6819e03a5b8
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-05-21 21:06:07 +0500

    Almost five question done to chpter is 21 to 25

---
+++ app.js
@@ -0,0 +1,41 @@
+// var userFirst = prompt("Fisrt Name")
+// var userSecond = prompt("Last Name")
+// var fullName = userFirst + " " + userSecond 
+// document.write("Hello Good Morning"+ ' ' +  "<b>"+ fullName.toUpperCase() +"</b>")
+
+
+// var user = prompt("What is Your Favorite Mobile Phone Model")
+// document.write("<br>" + "Your Favorite Mobile Phone Model Is : " + "<b>" + user.toUpperCase() + "</b>" + "<br />" + "Your Length is : " + "<b>" + user.length + "</b>")
+
+
+
+
+// var usr = prompt("Search Name like a 'Pakistani..'")
+// document.write("<br>" + "Your Search Word Is " + "<b>" + usr.toUpperCase() + "</b>" + "<br />" + "Index of 'n' " + usr.indexOf("n"))
+
+
+
+// var user = prompt("Search like a 'Hello World' ")
+// document.write("<br>" + "you search is " + "<b>" + user.toUpperCase() + "</b>" +'<br>'+ "length of 'L' is " + user.lastIndexOf("l"))
+
+// var user = prompt("Search any word")
+// document.write("<br>" + "Your search word is " + "<b>" + user.toUpperCase() + "</b>" + "<br />" + "Character Of Index 3rd : " + user.charAt(3))
+
+
+
+
+
+
+
+// var userFirst = prompt("Fisrt Name")
+// var userSecond = prompt("Last Name")
+// var fullName = userFirst + " " + userSecond 
+// document.write("your name is " + fullName.concat())
+
+
+
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName.concat(" ",lastName);
+// document.write("<br>" + "Full Name using string concat function: " + fullName);
